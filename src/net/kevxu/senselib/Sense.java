6d32baae00c203d69a0987d4c141a87f20f4e0f2
Kaiwen Xu <xu227@purdue.edu>
2013-02-27 16:16:21 -0500

    Building basic structure of LocationService and SensorService.

--- src/net/kevxu/senselib/Sense.java
+++ src/net/kevxu/senselib/Sense.java
@@ -5,9 +5,13 @@ import android.content.Context;
 public class Sense {
 
 	private Context mContext;
+	private LocationService mLocationService;
+	private SensorService mSensorService;
 
 	public Sense(Context context) {
 		mContext = context;
+		mLocationService = new LocationService(mContext);
+		mSensorService = new SensorService(mContext);
 
 	}
 
--- src/net/kevxu/senselib/SensorService.java
+++ src/net/kevxu/senselib/SensorService.java
@@ -1,5 +1,18 @@
 package net.kevxu.senselib;
 
+import android.content.Context;
+import android.hardware.SensorManager;
+
 public class SensorService {
+
+	private Context mContext;
+	private SensorManager mSensorManager;
+
+	protected SensorService(Context context) {
+		mContext = context;
+		mSensorManager = (SensorManager) mContext
+				.getSystemService(Context.SENSOR_SERVICE);
+
+	}
 
 }
