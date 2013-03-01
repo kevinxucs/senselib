b619ebf5425daa32cba904146aa01dc8b9a0c552
Kaiwen Xu <kevin@kevxu.net>
2013-03-01 14:11:33 -0500

    StepDetector, start from scratch, not using Levente Bagi's code at all.

--- src/net/kevxu/senselib/StepDetector.java
+++ src/net/kevxu/senselib/StepDetector.java
@@ -1,40 +1,35 @@
-/*
- *  Pedometer - Android App
- *  Copyright (C) 2009 Levente Bagi
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package net.kevxu.senselib;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class StepDetector implements SensorEventListener {
+import android.content.Context;
+import android.hardware.SensorManager;
 
-	@Override
-	public void onAccuracyChanged(Sensor arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
+public class StepDetector {
+	private final static String TAG = "StepDetector";
+
+	private Context mContext;
+	private SensorManager mSensorManager;
+	private List<StepListener> mStepListeners;
+
+	public interface StepListener {
+		public void onStep();
 	}
 
-	@Override
-	public void onSensorChanged(SensorEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public StepDetector(Context context) {
+		mContext = context;
+		mSensorManager = (SensorManager) mContext
+				.getSystemService(Context.SENSOR_SERVICE);
+		mStepListeners = new ArrayList<StepListener>();
 	}
 
+	public void addListener(StepListener stepListener) {
+		mStepListeners.add(stepListener);
+	}
+
+	public void removeListeners() {
+		mStepListeners.clear();
+	}
 
 }
