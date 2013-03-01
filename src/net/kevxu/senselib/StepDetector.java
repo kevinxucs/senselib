5622249bc46e995d7365bce0d05e9c7ba55d2442
Kaiwen Xu <kevin@kevxu.net>
2013-03-01 15:54:05 -0500

    Buidling listeners for StepDetector.

--- src/net/kevxu/senselib/StepDetector.java
+++ src/net/kevxu/senselib/StepDetector.java
@@ -3,8 +3,11 @@ package net.kevxu.senselib;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.kevxu.senselib.shared.SensorNotAvailableException;
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 public class StepDetector {
@@ -16,18 +19,43 @@ public class StepDetector {
 
 	private Sensor mLinearAccelSensor;
 	private Sensor mGravitySensor;
+	private LinearAccelSensorListener mLinearAccelSensorListener;
+	private GravitySensorListener mGravitySensorListener;
 
 	public interface StepListener {
 		public void onStep();
 	}
 
-	public StepDetector(Context context) {
+	public StepDetector(Context context) throws SensorNotAvailableException {
 		mContext = context;
 		mSensorManager = (SensorManager) mContext
 				.getSystemService(Context.SENSOR_SERVICE);
 
 		List<Sensor> liearAccelSensors = mSensorManager
 				.getSensorList(Sensor.TYPE_LINEAR_ACCELERATION);
+		List<Sensor> gravitySensors = mSensorManager
+				.getSensorList(Sensor.TYPE_GRAVITY);
+
+		if (liearAccelSensors.size() == 0) {
+			throw new SensorNotAvailableException(
+					"Linear Acceleration sensors not available.");
+		} else {
+			mLinearAccelSensor = liearAccelSensors.get(0);
+		}
+
+		if (gravitySensors.size() == 0) {
+			throw new SensorNotAvailableException(
+					"Gravity sensors not available.");
+		} else {
+			mGravitySensor = gravitySensors.get(0);
+		}
+
+		mLinearAccelSensorListener = new LinearAccelSensorListener();
+		mGravitySensorListener = new GravitySensorListener();
+		mSensorManager.registerListener(mLinearAccelSensorListener,
+				mLinearAccelSensor, SensorManager.SENSOR_DELAY_GAME);
+		mSensorManager.registerListener(mGravitySensorListener, mGravitySensor,
+				SensorManager.SENSOR_DELAY_GAME);
 
 		mStepListeners = new ArrayList<StepListener>();
 	}
@@ -40,4 +68,39 @@ public class StepDetector {
 		mStepListeners.clear();
 	}
 
+	public void close() {
+		mSensorManager.unregisterListener(mLinearAccelSensorListener);
+		mSensorManager.unregisterListener(mGravitySensorListener);
+	}
+
+	public void reload() {
+		mSensorManager.registerListener(mLinearAccelSensorListener,
+				mLinearAccelSensor, SensorManager.SENSOR_DELAY_GAME);
+		mSensorManager.registerListener(mGravitySensorListener, mGravitySensor,
+				SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	private class LinearAccelSensorListener implements SensorEventListener {
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+
+		}
+	}
+
+	private class GravitySensorListener implements SensorEventListener {
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+
+		}
+	}
 }
