eb47228dc08dc0714a10875ce3622488ce0fff3c
Kaiwen Xu <kevin@kevxu.net>
2013-03-15 00:16:25 -0400

    Add strong notice to DataPool class to emphasize it's not thread-safe.

--- src/net/kevxu/senselib/DataPool.java
+++ src/net/kevxu/senselib/DataPool.java
@@ -7,8 +7,8 @@ import java.util.Random;
 /**
  * Data container which stores last n data, n is defined by poolSize. It uses
  * the concept of circular array, but the underlining data container uses
- * ArrayList due to the need of support generic. This data container is not
- * thread-safe, use with caution.
+ * ArrayList due to the need of support generic. This data container is NOT
+ * thread-safe, use with ABSOLUTE caution.
  * 
  * @author Kaiwen Xu
  * 
