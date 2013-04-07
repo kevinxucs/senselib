625f0cf6e00df941f95f5c6f8504894afdc6df29
Kaiwen Xu <kevin@kevxu.net>
2013-04-07 17:03:58 -0400

    Add ability to choose enable what services.
    Add constructor to accept bitmasked argument for determining which
    services should be enabled.

--- src/net/kevxu/senselib/Sense.java
+++ src/net/kevxu/senselib/Sense.java
@@ -1,5 +1,8 @@
 package net.kevxu.senselib;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
 
 /**
@@ -12,31 +15,75 @@ import android.content.Context;
  * @author Kaiwen Xu
  */
 public class Sense {
+	
+	/**
+	 * Orientation service.
+	 */
+	public static final int SERVICE_ORIENTATION = 0x1;
+	
+	/**
+	 * Step detector service. Since it relies on orientation service, orientation
+	 * service is implied.
+	 */
+	public static final int SERVICE_STEP_DETECTOR = 0x3;
+	
+	/**
+	 * Location service. Since it relies on both orientation service and step
+	 * detector service, both of them are implied.
+	 */
+	public static final int SERVICE_LOCATION = 0x7;
+	
+	/**
+	 * All services available.
+	 */
+	public static final int SERVICE_ALL = 0xFFFFFFFF;
 
 	private Context mContext;
+	
+	private List<SensorService> mServices;
 
 	private OrientationService mOrientationService;
 	private StepDetector mStepDetector;
 	private LocationService mLocationService;
-
+	
 	public Sense(Context context) throws SensorNotAvailableException {
+		this(context, SERVICE_ALL);
+	}
+	
+	public Sense(Context context, int services) throws SensorNotAvailableException {
 		mContext = context;
+		mServices = new ArrayList<SensorService>();
 
-		mOrientationService = new OrientationService(mContext);
-		mStepDetector = new StepDetector(mContext, mOrientationService);
-		mLocationService = new LocationService(mContext, mStepDetector);
+		if ((services & SERVICE_ORIENTATION) == SERVICE_ORIENTATION) {
+			mOrientationService = new OrientationService(mContext);
+			mServices.add(mOrientationService);
+		}
+		
+		if ((services & SERVICE_STEP_DETECTOR) == SERVICE_STEP_DETECTOR) {
+			mStepDetector = new StepDetector(mContext, mOrientationService);
+			mServices.add(mStepDetector);
+		}
+		
+		if ((services & SERVICE_LOCATION) == SERVICE_LOCATION) {
+			mLocationService = new LocationService(mContext, mStepDetector);
+			mServices.add(mLocationService);
+		}
 	}
 
 	public void start() {
-		mOrientationService.start();
-		mStepDetector.start();
-		mLocationService.start();
+		for (SensorService service : mServices) {
+			if (service != null) {
+				service.start();
+			}
+		}
 	}
 
 	public void stop() {
-		mLocationService.stop();
-		mStepDetector.stop();
-		mOrientationService.stop();
+		for (SensorService service : mServices) {
+			if (service != null) {
+				service.stop();
+			}
+		}
 	}
 
 	public OrientationService getOrientationServiceInstance() {
